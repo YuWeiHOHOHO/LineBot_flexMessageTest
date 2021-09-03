ce4730ee489d5dbcb2abe3ad93d6a0682024bbe5
YuWei <pengurn89@gmail.com>
2021-09-03 14:37:57 +0800

    imgSearch_wakeup_toSeeDatatime

--- clock.py
+++ clock.py
@@ -2,11 +2,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', minute='*/1')
+@sched.scheduled_job('cron', minute='*/2')
 def scheduled_job():
     print('========== APScheduler CRON =========')
     # 馬上讓我們瞧瞧
-    print('This job runs every day */1 min.')
+    print('This job runs every day */2 min.')
     # 利用datetime查詢時間
     print(f'{datetime.datetime.now().ctime()}')
     print('========== APScheduler CRON =========')
