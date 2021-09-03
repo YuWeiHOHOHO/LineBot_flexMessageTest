f1edfcf1c8a660be5bcc36ba2d31929890112b7e
YuWei <pengurn89@gmail.com>
2021-09-03 15:40:27 +0800

    imgSearch_wakeup_askForLineID

--- app_core.py
+++ app_core.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 import os
+
+# 增加了 render_template
 from flask import Flask, request, abort, render_template
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
@@ -20,10 +23,10 @@ config.read('config.ini')
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
-#為了使heroku不要睡，定時給予呼喚
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home_pixijs.html")
+
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -45,6 +48,7 @@ def callback():
 def pixabay_isch(event):
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        # 先找圖
         try:
             url = f"https://pixabay.com/images/search/{urllib.parse.urlencode({'q':event.message.text})[2:]}/"
             headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
@@ -54,13 +58,13 @@ def pixabay_isch(event):
             
             print('fetch page finish')
             
-            pattern = 'img src="\S*"'
+            pattern = 'img srcset="\S*\s\w*,'
             img_list = []
             
             for match in re.finditer(pattern, str(conn.read())):
                 img_list.append(match.group()[12:-3])
                 
-            random_img_url = img_list[random.randint(0, len(img_list)+1)]
+            random_img_url = random.choice(img_list)
             print('fetch img url finish')
             print(random_img_url)
             
@@ -71,13 +75,12 @@ def pixabay_isch(event):
                     preview_image_url=random_img_url
                 )
             )
-        # 如果找不到圖，就學你說話
+        # 找不到圖就告訴我 user_id
         except:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="Search Error")
+                TextSendMessage(text=str(event.source.user_id))
             )
-            pass
 
 if __name__ == "__main__":
     app.run()
