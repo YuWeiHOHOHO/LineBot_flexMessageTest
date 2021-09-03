d1d06d349ea7d976fa709ed2ad4ff19eb2b888bc
YuWei <pengurn89@gmail.com>
2021-09-03 14:23:11 +0800

    imgSearch_wakeup_toSeeDatatime

--- clock.py
+++ clock.py
@@ -2,12 +2,17 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
+@sched.scheduled_job('cron', minute='*/1')
 def scheduled_job():
-    url = "https://你-APP-的名字.herokuapp.com/"
+    print('========== APScheduler CRON =========')
+    # 馬上讓我們瞧瞧
+    print('This job runs every day */1 min.')
+    # 利用datetime查詢時間
+    print(f'{datetime.datetime.now().ctime()}')
+    print('========== APScheduler CRON =========')
+
+    url = "https://yuweiweb.herokuapp.com/"
     conn = urllib.request.urlopen(url)
         
     for key, value in conn.getheaders():
         print(key, value)
-
-sched.start()
